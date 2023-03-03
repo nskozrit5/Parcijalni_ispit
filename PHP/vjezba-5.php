57ef29cd02b06be9b0c55a287d44acf6f2d5048c
Nino Skozrit <nino.skozrit@gmail.com>
2023-03-03 20:13:21 +0000

    PHP-3

--- PHP/vjezba-5.php
+++ PHP/vjezba-5.php
@@ -4,7 +4,7 @@
   $b = 10;
   $c = 15;
 
-  if ((($b > $a) & ($b < $c)) || (($b < $a) & ($b > $c))){
+  if ((($b >= $a) & ($b <= $c)) || (($b <= $a) & ($b >= $c))){
     print_r("b je između \n");
   } else {
     print_r("b nije između \n");
