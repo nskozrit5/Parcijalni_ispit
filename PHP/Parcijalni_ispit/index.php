4f141693b7bee3430d9b7db1c071cc69ca2d6f93
Nino Skozrit <nino.skozrit@gmail.com>
2023-03-23 18:48:34 +0100

    PHP-9

--- PHP/Parcijalni_ispit/index.php
+++ PHP/Parcijalni_ispit/index.php
@@ -3,55 +3,120 @@
 <head>
   <meta charset="UTF-8">
   <title>Parcijalni ispit</title>
+  <style>
+    div{
+      display:flex;
+      justify-content: space-around;
+    }
+    table, th, td{
+      border: 1px solid black;
+    }
+    #poruka{
+      padding-top: 50px;
+      padding-left: 50px;
+      float: left;
+    }
+  </style>
+
 </head>
 <body>
-  <form action="<?php $_SERVER['PHP_SELF'] ?>" method="get">
-    <input type="text" placeholder="Unesi riječ" name="rijec">
-    <input type="submit" value="Provjeri" name="unos">
-  </form> 
+  <div>
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="get">
+      <input type="text" placeholder="Unesi riječ" name="rijec">
+      <br>
+      <input type="submit" value="Pošalji" name="unos">
+      <br>
+    </form>
+<br>
+<?php
+/*----------------ispis json------------------*/
+$rijeciJson = file_get_contents('words.json'); //čitanje sadržaja json-a
+$rijeci = json_decode($rijeciJson, true); //stavljanje u niz
+
+$i = 0;
+echo "<table>
+        <tr>
+          <th>Riječ</th>
+          <th>Broj slova</th>
+          <th>Broj suglasnika</th>
+          <th>Broj samoglasnika</th>
+        </tr>
+";
+if (!empty($rijeci)) {
+    foreach ($rijeci as $rijec) {
+        echo "<tr>";
+        echo "<td>" . $rijeci[$i]['rijec'] . "</td>";
+        echo "<td>" . $rijeci[$i]['brojSlova'] . "</td>";
+        echo "<td>" . $rijeci[$i]['brojSuglasnika'] . "</td>";
+        echo "<td>" . $rijeci[$i]['brojSamoglasnika'] . "</td>";
+        echo "</tr>";
+        $i++;
+    }
+} else {
+    $ispis = "Nijedna riječ nije unesena!";
+}
+echo "</table>";
+/*----------------ispis json------------------*/
+?>
+  </div>
+  <div id="poruka">
+<?php
+if (!empty($ispis)) {
+    echo $ispis;
+}
+?>
+  </div>
 </body>
 </html>
 
-
 <?php
 /*----------------funkcije------------------*/
-function samoglasnici(){
-  global $rijec;
-  global $niz;
-  global $broj_slova;
-  $samoglasnici = ['a','e','i','o','u'];
-  $broj_samoglasnika = 0;
-  foreach ($niz as $slovo) {
-      for ($i = 0; $i < 5; $i++) {
-          if ($slovo == $samoglasnici[$i]) {
-              $broj_samoglasnika++;
-          }
-      }
-  }
-  $broj_suglasnika = $broj_slova - $broj_samoglasnika;
-  echo $broj_samoglasnika;
+function samoglasnici()
+{
+    global $niz;
+    $samoglasnici = ['a', 'e', 'i', 'o', 'u'];
+    $broj_samoglasnika = 0;
+    foreach ($niz as $slovo) {
+        for ($i = 0; $i < 5; $i++) {
+            if ($slovo == $samoglasnici[$i]) {
+                $broj_samoglasnika++;
+            }
+        }
+    }
+    return $broj_samoglasnika;
 }
 
-function suglasnici(){
-  global $broj_slova;
-  global $broj_samoglasnika;
-  $broj_suglasnika = $broj_slova - $broj_samoglasnika;
-  echo $broj_suglasnika;
+function suglasnici($broj_samoglasnika)
+{
+    global $broj_slova;
+    $broj_suglasnika = $broj_slova - $broj_samoglasnika;
+    return $broj_suglasnika;
 }
 /*----------------funkcije------------------*/
 
 if (isset($_GET['unos'])) {
-  $rijec = $_GET['rijec'];
-  if(!empty($rijec)){
-    $rijec = strtolower($rijec); //mala slova
-    $broj_slova = strlen($rijec); //broj slova
-    echo "$broj_slova <br>";
-    $niz = str_split($rijec); //prebacivanje u niz
-    samoglasnici(); //poziv funkcije
-    suglasnici(); //poziv funkcije
-  } else {
-    echo "Niste unijeli riječ!";
-  }
-}
+    $rijec = $_GET['rijec'];
+    if (!empty($rijec)) {
+        $rijec = strtolower($rijec); //mala slova
+        $broj_slova = strlen($rijec); //broj slova
+        echo "Broj slova: $broj_slova <br>";
+        $niz = str_split($rijec); //prebacivanje u niz
 
+        /*----------------unosjson------------------*/
+        $rijeci[] = [
+            "rijec" => $rijec,
+            "brojSlova" => $broj_slova,
+            "brojSamoglasnika" => samoglasnici(),
+            "brojSuglasnika" => suglasnici(samoglasnici()),
+        ];
+        $rijeciJson = json_encode($rijeci); //transformiranje u JSON
+        file_put_contents('words.json', $rijeciJson); //zapisivanje u words.json
+        /*----------------unosjson------------------*/
+
+        header("Location: index.php"); //reload
+    } else {
+        echo "<br>Niste unijeli riječ!";
+        //$ispis1 = "Niste unijeli riječ!";
+    }
+}
 ?>
